818f19330db5a88cea67a7ff519c2bae4b33439e
ian hatcher-williams <ian@gardener.nyc>
2024-09-28 17:32:26 -0400

    redirect home for lander.

--- src/entrypoints/theme.js
+++ src/entrypoints/theme.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+// Code credit
+console.groupCollapsed('Site Credits');
+console.log('Design by Malu Marzarotto https://malu.work');
+console.log('Development by Gardener https://www.gardener.nyc');
+console.groupEnd();
